938b84fe6f63873a21e8b0dd885d74e45eac2477
Thomas Hutterer <thutterer@suse.de>
2019-12-15 11:51:56 +0100

    Add a dynamic counter column
    See https://datatables.net/examples/api/counter_columns.html

--- app/assets/javascripts/mr_video/application.js
+++ app/assets/javascripts/mr_video/application.js
@@ -15,7 +15,14 @@
 //= require_tree ./dataTables
 
 $(document).ready(function() {
-  $(".table").DataTable({
+  var t = $(".table").DataTable({
     "autoWidth": false,
+    "order": [[1, 'asc']]
   });
+
+  t.on( 'order.dt search.dt', function() {
+    t.column(0, {search:'applied', order:'applied'}).nodes().each(function(cell, i) {
+      cell.innerHTML = i+1;
+    });
+  }).draw();
 });
